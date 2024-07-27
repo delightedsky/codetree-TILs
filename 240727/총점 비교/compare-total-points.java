8e0ce6bc8fa63f1db1689edd61e22b4feee193e8
delightedsky <145816805+delightedsky@users.noreply.github.com>
2024-07-27 22:03:04 +0900

    Title: 총점 비교, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240727/총점 비교/compare-total-points.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+// 학생들의 정보를 나타내는 클래스 선언
+class Student implements Comparable<Student> {
+    String name;
+    int korean, english, math;
+
+    public Student(String name, int korean, int english, int math){
+        this.name = name;
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        // 세 점수의 합이 오름차순이 되도록 정렬합니다.
+        return (this.korean + this.english + this.math) - 
+               (student.korean + student.english + student.math);
+    }
+};
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+        for(int i = 0; i < n; i++) {
+            String name = sc.next();
+            int korean = sc.nextInt();
+            int english = sc.nextInt();
+            int math = sc.nextInt();
+            students[i] = new Student(name, korean, english, math);
+        }
+
+        Arrays.sort(students);
+        for (int i = 0; i < n; i++){
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].korean + " ");
+            System.out.print(students[i].english + " ");
+            System.out.println(students[i].math);
+        }
+    }
+}
