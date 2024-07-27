73b200333d1419f778a4a1f38b8af1f90c7a4a2b
delightedsky <145816805+delightedsky@users.noreply.github.com>
2024-07-27 22:00:50 +0900

    Title: 키를 기준으로 정렬, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240727/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+// 학생들의 정보를 나타내는 클래스 선언
+class Student implements Comparable<Student> {
+    String name;
+    int height, weight;
+
+    public Student(String name, int height, int weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Student student) { 
+        return this.height - student.height;
+    }
+};
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+        for(int i = 0; i < n; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+
+            students[i] = new Student(name, height, weight);
+        }
+
+        Arrays.sort(students);
+
+        for (int i = 0; i < n; i++){
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].height + " ");
+            System.out.println(students[i].weight);
+        }
+    }
+}
