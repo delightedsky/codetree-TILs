77811e009b644d050fc002275e484fdf4f4f4505
delightedsky <145816805+delightedsky@users.noreply.github.com>
2024-08-09 23:49:45 +0900

    Title: 일렬로 서있는 소 2, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240809/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+        
+        int count = 0;
+        
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    if (A[i] <= A[j] && A[j] <= A[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
